37fd29e621b6bf4e2c57a63a76c8cb397fa07440
goisygor <ygorgoissenai@gmail.com>
2024-05-16 20:02:26 -0300

    ok

---
+++ deploy/src/main/java/br/com/luishb/lojalivro/Controller/IndexController.java
@@ -0,0 +1,22 @@
+package br.com.luishb.lojalivro.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public ModelAndView getIndex () {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+    @GetMapping("/home")
+    public ModelAndView getHome() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+    
+}
--- deploy/src/main/java/br/com/luishb/lojalivro/Controller/LivrosController.java
+++ deploy/src/main/java/br/com/luishb/lojalivro/Controller/LivrosController.java
@@ -2,20 +2,19 @@ package br.com.luishb.lojalivro.Controller;
 
 
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import br.com.luishb.lojalivro.Model.Livro;
 import br.com.luishb.lojalivro.Repository.LivroRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -25,15 +24,18 @@ public class LivrosController {
     @Autowired
     LivroRepository livrosRepository;
 
-    @GetMapping("/livros")
+    @GetMapping("/livros-list")
     public List<Livro> list() {
-        return (List<Livro>) this.livrosRepository.findAll();
+        return (List<Livro>) this.livrosRepository.findAll();    
     }
 
-    @PostMapping("/livros")
-    public Livro create(@RequestBody Livro livro){
+    @PostMapping("/livros-add")
+    public Livro create(@RequestBody Livro livro) {
         return this.livrosRepository.save(livro);
     }
+    
+
+
     
 
 }
