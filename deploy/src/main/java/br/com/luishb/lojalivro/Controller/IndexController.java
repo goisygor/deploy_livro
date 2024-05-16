5192e9da79add86e69386c96912a35a6b53a2e0b
goisygor <ygorgoissenai@gmail.com>
2024-05-16 20:54:34 -0300

    ok

--- deploy/src/main/java/br/com/luishb/lojalivro/Controller/IndexController.java
+++ deploy/src/main/java/br/com/luishb/lojalivro/Controller/IndexController.java
@@ -5,7 +5,6 @@ package br.com.luishb.lojalivro.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
--- deploy/src/main/java/br/com/luishb/lojalivro/Controller/LivroController.java
+++ deploy/src/main/java/br/com/luishb/lojalivro/Controller/LivroController.java
@@ -1,9 +1,6 @@
 package br.com.luishb.lojalivro.Controller;
 
 
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +10,12 @@ import br.com.luishb.lojalivro.Model.Livro;
 import br.com.luishb.lojalivro.Repository.LivroRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-import java.util.List;
+
 
 
 
@@ -30,14 +27,17 @@ public class LivroController {
     LivroRepository livrosRepository;
 
     @GetMapping("/livros-list")
-    public List<Livro> list() {
-        return (List<Livro>) this.livrosRepository.findAll();    
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("/livros-list");
+        mv.addObject("livros", livrosRepository.findAll());
+        return mv ;
     }
 
     @PostMapping("/livros-add")
     public ModelAndView create(@RequestBody Livro livro) {
         ModelAndView mv = new ModelAndView("livros-add");
         livrosRepository.save(livro);
+        mv.setViewName("redirect:/livros-list");
         return mv;
     }
     
